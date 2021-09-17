5f9d535d53bf19e614c82283c17dcdc23f47bd01
derrickmacharia <derrick.macharia@student.moringaschool.com>
2021-09-17 14:53:09 +0300

    add and style the home section

---
+++ js/home.js
@@ -0,0 +1,7 @@
+const hero = document.querySelector(".hero")
+const slider = document.querySelector(".slider")
+const logo = document.querySelector(".logo")
+const navigation = document.querySelector(".navigation")
+const headline = document.querySelector(".headline")
+
+
